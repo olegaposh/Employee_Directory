fe50c7e62e17b5c8f4a496430bfa1974dd0cbdf1
olegaposh <oleg.poshiv@gmail.com>
2020-07-14 16:00:50 -0700

    filter search now working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Header from './components/Header/header';
 import Search from './components/Search/Search';
-import Main from './components/Main/Main'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import friends from './friends.json';
 
 
-// console.log(friends)
+
+
 function App() {
   return (
     <Container>
       <Header />
       <Search />
-      <Main />
+      {/* <Main /> */}
     </Container>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,33 +6,33 @@ import Table from 'react-bootstrap/Table'
 
 
 const Main = (props) => {
-    console.log(friends)
-    return (
-        <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>Image</th>
-      <th>Name</th>
-      <th>Phone</th>
-      <th>Email</th>
-      <th>DOB</th>
-    </tr>
-  </thead>
-  <tbody>
-      
-    {friends.map(item => 
-        (<tr key={item.id}>
+  // console.log(friends)
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>DOB</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        {friends.map(item =>
+          (<tr key={item.id}>
             <td><Image className="img" src={item.image} thumbnail /></td>
             <td>{item.name}</td>
             <td>{item.phone}</td>
             <td>{item.email}</td>
             <td>{item.DOB}</td>
-        </tr>))}
-        
-  </tbody>
-</Table>
-        
-    )
+          </tr>))}
+
+      </tbody>
+    </Table>
+
+  )
 
 }
 
