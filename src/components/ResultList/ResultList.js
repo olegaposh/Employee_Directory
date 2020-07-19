475b815e177ee91fbc1b3113fbecffb72390df6b
olegaposh <oleg.poshiv@gmail.com>
2020-07-18 21:15:47 -0700

    formatted date of birth

--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -4,7 +4,17 @@ import Image from 'react-bootstrap/Image'
 import Table from 'react-bootstrap/Table'
 
 
+
 const ResultList = (props) => {
+
+
+const formatDate = (d) => {
+
+  var date = new Date(d);
+  return(((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear());
+
+}
+  
   if (props.results.length > 0) {
     // console.log(props.results[0].id.value)
     // console.log(props.results[0].picture.thumbnail)
@@ -33,7 +43,7 @@ const ResultList = (props) => {
             <td>{item.name.first} {item.name.last}</td>
             <td>{item.cell}</td>
             <td>{item.email}</td>
-            <td>{item.dob.date}</td>
+            <td>{formatDate(item.dob.date)}</td>
           </tr>))} 
 
       </tbody>
--- src/components/SearchContainer/SearchContainer.js
+++ src/components/SearchContainer/SearchContainer.js
@@ -21,11 +21,10 @@ class SearchContainer extends Component {
         try {
             const result = await axios.get(this.URL)
             // update the state of results
-
-            console.log("hello", result)
+            
             this.setState({ results: result.data.results })
-            
-            
+
+
         } catch (error) {
             console.log(error)
         }
@@ -33,7 +32,7 @@ class SearchContainer extends Component {
 
     handleInputChange = event => {
 
-        const {name, value} = event.target
+        const { name, value } = event.target
         // console.log(event.target.name)
         this.setState({
             [name]: value
@@ -45,42 +44,49 @@ class SearchContainer extends Component {
         this.searchUser();
     }
 
-    // handleclick = event => {
-    //     this.state.results.sort(function(dataA, dataB) {
-    //         return compareStrings(dataA.name.first, dataB.name.first);
-    //       })
-    //     }
-    // }
+    compareStrings = (a, b) => {
+        // Assuming you want case-insensitive comparison
+        a = a.toLowerCase();
+        b = b.toLowerCase();
 
-    // function compareStrings(a, b) {
-    //     // Assuming you want case-insensitive comparison
-    //     a = a.toLowerCase();
-    //     b = b.toLowerCase();
-      
-    //     return (a < b) ? -1 : (a > b) ? 1 : 0;
-    //   }
-      
-      
+        return (a < b) ? -1 : (a > b) ? 1 : 0;
+    }
+
+    handleClick = event => {
+        console.log(this.state.results)
+        this.state.results.sort((dataA, dataB) => {
+            return this.compareStrings(dataA.name.first, dataB.name.first);
+        })
+    }
+    
+    // handleClick = event => {
+    //     this.state.results.sort()
+    //     }
+    
+
+
+
 
     render() {
 
         const filteredSearch = this.state.results.filter((data) => {
-           
-        if(this.state.search === "")
-        return data
-        else if(data.name.first.toLowerCase().includes(this.state.search.toLowerCase()))
-        return data
-        else if(data.name.last.toLowerCase().includes(this.state.search.toLowerCase()))
-        return data
-        
-        
-    })
-    // console.log(filteredSearch)
+
+            if (this.state.search === "")
+                return data
+            else if (data.name.first.toLowerCase().includes(this.state.search.toLowerCase()))
+                return data
+            else if (data.name.last.toLowerCase().includes(this.state.search.toLowerCase()))
+                return data
+
+
+        })
+        // console.log(filteredSearch)
         return (
             <>
-            {/* <button onClick={() => this.handleClick()}
-            >SortME!</button> */}
-                <Search 
+                <button 
+                onClick={() => this.handleClick()}>SORT
+                </button>
+                <Search
                     search={this.state.search}
                     handleInputChange={this.handleInputChange}
                 />
