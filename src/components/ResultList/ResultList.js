fb091c68b38f22fbb370edfc1363bb09293680b2
olegaposh <oleg.poshiv@gmail.com>
2020-07-19 23:10:25 -0700

    test

--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -25,9 +25,10 @@ const formatDate = (d) => {
   }
   
   return (
+    <>
     <Table striped bordered hover>
       <thead>
-        <tr>
+        <tr class="black white-text">
           <th>Image</th>
           <th>Name</th>
           <th>Phone</th>
@@ -49,6 +50,72 @@ const formatDate = (d) => {
       </tbody>
     </Table>
 
+    <table class="table">
+  <thead class="black white-text">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="table">
+  <thead class="grey lighten-2">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>3
+
+
+
+
+</>
   )
 
 }
